a0dd06f4f893f0adb4e7aa8ca528549040d5cc1a
DerDawo <71173937+DerDawo@users.noreply.github.com>
2023-12-28 18:20:57 +0000

    added simulation Class, seperated update and draw

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Triangle{
         ctx.closePath();
 
         // the outline
-        ctx.lineWidth = 10;
+        ctx.lineWidth = 1;
         ctx.strokeStyle = this.stroke_color;
         ctx.stroke();
 
@@ -25,26 +25,49 @@ class Triangle{
         ctx.fillStyle = this.stroke_color;
         ctx.fill();
     }
+
+    update(environment){
+        for (const vertex of this.vertices){
+            vertex.update(environment)
+        }
+    }
 }
 
 class Vertex{
-    constructor(x,y,dx,dy,radius,color){
+    constructor(x,y,dx,dy,ax,ay,radius,color){
         //Position
         this.x = x,
         this.y = y,
-        //Movement
+        //Distance per Animation Draw
         this.dx = dx,
         this.dy = dy,
+        //Acceleration per Animation
+        this.ax = ax,
+        this.ay = ay,
         //Radius
         this.radius = radius,
         //Color
         this.color = color
     }
 
-    
-    move(environment){
-        const x_is_out_of_bounds = (this.x - this.radius + this.dx < 0 || this.x + this.radius  + this.dx > environment.width)
-        const y_is_out_of_bounds = (this.y - this.radius + this.dy < 0 || this.y + this.radius  + this.dy > environment.height)
+    draw(ctx){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.stroke();
+
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }    
+
+    update(environment){
+        let next_position_in_x = this.x + this.dx
+        let next_position_in_y = this.y + this.dy
+
+        const x_is_out_of_bounds = (next_position_in_x - this.radius < 0 || next_position_in_x + this.radius > environment.width)
+        const y_is_out_of_bounds = (next_position_in_y - this.radius < 0 || next_position_in_y + this.radius > environment.height)
         
         if(x_is_out_of_bounds){
             this.dx = this.dx * (-1)
@@ -54,26 +77,66 @@ class Vertex{
             this.dy = this.dy * (-1)
         }
 
-        this.x += this.dx
-        this.y += this.dy
-    }
-    
-    draw(ctx){
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.strokeStyle = "black";
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
-    }
+        next_position_in_x = this.x + this.dx
+        next_position_in_y = this.y + this.dy
 
+        this.x = next_position_in_x
+        this.y = next_position_in_y
+    }
 }
 
 class Environment{
     constructor(htmlElement){
         this.HTML = htmlElement,
         this.width = this.HTML.getBoundingClientRect().width,
-        this.height = this.HTML.getBoundingClientRect().height
+        this.height = this.HTML.getBoundingClientRect().height,
+        this.members = []
+    }
+
+    setMembers(members){
+        this.members.push.apply(this.members,members);
+    }
+
+    draw(){
+        const ctx = this.HTML.getContext("2d");
+        let w = this.width
+        let h = this.height
+        ctx.globalCompositeOperation = "destination-over";
+        ctx.clearRect(0, 0, w, h); // clear canvas
+        
+        for (const m of this.members){
+            m.draw(ctx)
+        }
+    }
+
+    update(){
+        for (const m of this.members){
+            m.update(this)
+        }
+    }
+}
+
+class Simulation{
+    constructor(){
+        this.environment = null
+    }
+    
+    setEnvironment(environment){
+        this.environment = environment
+    }
+
+    start(){
+
+        if (this.environment == null){
+            console.warn("No Environment set!")
+            return
+        }
+
+        window.requestAnimationFrame(this.run)
+    }
+
+    run(){
+        window.requestAnimationFrame(this.run.bind(this))
     }
 }
 
@@ -89,38 +152,35 @@ const colors = ['red','blue','green','yellow','orange','brown','purple','aqua']
 const number_of_particles = 50
 const particles = []
 for (let i = 0; i<number_of_particles; i++){
-    let x = generateRandom(50,350)
-    let y = generateRandom(50,350)
+    let x = generateRandom(200,300)
+    let y = generateRandom(200,300)
     let dx = generateRandom(-5,5)
     let dy = generateRandom(-5,5)
-    let radius = 10
-    let color = colors[Math.floor(Math.random()*colors.length)];
+    let ax = generateRandom(-5,5)
+    let ay = generateRandom(-5,5)
+    let radius = 20
+    let color = colors[generateRandom(0,colors.length-1)];
+    //let color = colors[Math.floor(Math.random()*colors.length)];
 
-    let particle = new Vertex(x,y,dx,dy,radius,color)
+    let particle = new Vertex(x,y,dx,dy,ax,ay,radius,color)
     particles.push(particle)
 }
 
+const vertex_0 = new Vertex(50,50,1,0.43762,1,0,0,"black")
+const vertex_1 = new Vertex(25,50,-0.5,-0.4362,1,0,0,"black")
+const vertex_2 = new Vertex(50,25,-1,0.5,1,0,0,"black")
+const triangle = new Triangle(vertex_0,vertex_1,vertex_2,"#53ed4380","green")
+
+window.addEventListener("load",init)
 function init() {
-    window.requestAnimationFrame(draw);
-}
-
-function draw() {
-    const ctx = document.getElementById("canvas").getContext("2d");
     const canvas = document.getElementById("canvas");
+    console.log(canvas)
     const environment = new Environment(canvas)
-
-    ctx.globalCompositeOperation = "destination-over";
-    ctx.clearRect(0, 0, environment.width, environment.height); // clear canvas
-
-    for (const particle of particles){
-        particle.move(environment)
-        particle.draw(ctx)
-    }
-
-
-  window.requestAnimationFrame(draw);
+    //environment.setMembers([triangle])
+    environment.setMembers(particles)
+    const simulation = new Simulation()
+    simulation.setEnvironment(environment)
+    simulation.run()
 }
 
 
-
-init();
